a1257af0c7a01f2e0ab22675ac5aa894815697b8
jackup <yk.58@hotmail.com>
2021-12-06 17:14:36 +0300

    Add project files.

---
+++ MCD_OOPInterfaceNedir/IMusteri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPInterfaceNedir
+{
+    public interface IMusteri
+    {
+        //public IMusteri()
+        //{
+        // Interface içerisinde constructor kullanılamaz.
+        //}
+
+
+        //Field
+        int Id { get; set; }
+        string Isim { get; set; }
+        string Soyisim { get; set; }
+
+
+        //Metod
+        int YeniKayit(string isim, string soyisim);
+        int KayitDuzenle(int id, string isim, string soyisim);
+        int KayitSil(int id);
+
+
+    }
+}
---
+++ MCD_OOPInterfaceNedir/Musteri.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPInterfaceNedir
+{
+    public class Musteri : IMusteri
+    {
+        int id;
+        string isim, soyisim;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        public string Soyisim
+        {
+            get { return soyisim; }
+            set { soyisim = value; }
+        }
+
+        public int KayitDuzenle(int id, string isim, string soyisim)
+        {
+            Console.WriteLine("Kayıt düzenlendi..");
+            return 1;
+        }
+
+        public int KayitSil(int id)
+        {
+            Console.WriteLine("Kayıt silindi..");
+            return 1;
+        }
+
+        public int YeniKayit(string isim, string soyisim)
+        {
+            Console.WriteLine("Yeni kayıt eklendi..");
+            return 1;
+        }
+    }
+}
